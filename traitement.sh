f2a4e0e4742174e3fe18c9dd5fe4702a3b85a389
CUER alexandre <alexandre.cuer@cerema.fr>
2022-01-01 22:38:06 +0100

    Update traitement.sh

--- traitement.sh
+++ traitement.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-github=$pwd
-echo $github
-cd %1
+# ./traitement.sh /home/alexandrecuer/Documents/GitHub/portfolio/sources/balade1 vacances baladeEnHauteur 2021
+# pour installer convert : sudo apt install imagemagick-6.q16
 
-folder="${github}/images/%2/%4/%3"
-prefix=%2_%3
-echo $prefix
+repo=$(pwd)
+
+folder="${repo}/images/$2/$4/$3"
+prefix="$2_$3"
 echo $folder
 
+#cd $1
 if [ -d $folder ]; then
-    echo "I exist"
+    rm -Rf $folder
 fi
+mkdir $folder
+echo "an empty folder ${folder} has been created"
+
+src="images"
+nb=1
+for i in `ls ${src}`
+do
+  img="${src}/${i}"
+  if [ ! -d $img ]; then
+      echo $img
+      convert $img -resize 800x600 "${folder}/${prefix}_${nb}.jpg"
+      convert $img -resize 200x150 "${folder}/${prefix}_${nb}-thumb.jpg"
+      let nb+=1
+  fi
+done
+
 
