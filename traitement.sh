6d0a49d9e1574177512e530ad74cca1eb84322c5
CUER alexandre <alexandre.cuer@cerema.fr>
2022-01-02 21:26:42 +0100

    Update traitement.sh

--- traitement.sh
+++ traitement.sh
@@ -9,14 +9,13 @@ folder="${repo}/images/$2/$4/$3"
 prefix="$2_$3"
 echo $folder
 
-#cd $1
 if [ -d $folder ]; then
     rm -Rf $folder
 fi
 mkdir $folder
 echo "an empty folder ${folder} has been created"
 
-src="images"
+src="${1}"
 nb=1
 for i in `ls ${src}`
 do
@@ -29,4 +28,47 @@ do
   fi
 done
 
+land="{src}/landscape"
+if [ -d $land ]; then
+  for i in `ls ${land}`
+  do
+    img="{land}/${i}"
+    if [ ! -d $img ]; then
+        echo $img
+        convert $img -resize 800x600 "${folder}/${prefix}_${nb}.jpg"
+        convert $img -resize 100x75 "${folder}/${prefix}_${nb}-thumb.jpg"
+        let nb+=1
+    fi
+  done
+fi
 
+target="${repo}/_posts"
+now=$(date +%Y-%m-%d)
+postname="${target}/${now}-${prefix}.md"
+
+if [ -e $postname ]; then
+  rm -f $postname
+fi
+
+cat >> $postname <<EOF
+---
+layout: page
+header: no
+subheadline:  ${2}
+title: ${3}
+breadcrumb: true
+permalink: /${2}/${3}
+categories:
+    - ${2}
+tags:
+    - ${2}
+    - ${2}_${4}
+image:
+    base: ${2}/${4}/${3}/${2}_${3}
+    start: 1
+    max: ${nb}
+---
+{%% include gallery %%}
+{: .t60 }
+{%% include list-posts tag='${2}_${4}' %%}
+EOF
